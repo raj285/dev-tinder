e46ccd2acaeb5b8c5327c98fce7b2c7859c27377
NISHANT GOSWAMI <81947327+raj285@users.noreply.github.com>
2024-12-05 07:22:57 +0530

    exploring get put post

--- src/app.js
+++ src/app.js
@@ -4,11 +4,25 @@
 //  node modules and package lock json came
 //  
 const express = require('express');
-const app=express();
+const app=express(); 
 // server is responding , for all type of request
 // /test is route-> will work for local host:/test
-app.use("/test",(req,res)=>{
-    res.send("hello from the server");
+//**order matters alot  */
+// app.use("/test",(req,res)=>{
+//     res.send("hello from the server");
+//     // **above will work for /test/1,/test/e.....anything /test/+(...);**
+// });
+app.get("/test",(req,res)=>{
+    res.send({
+        firstname:"raj",
+        lastname:"Goswami"
+    });
+})
+app.post("/test", (req, res) => {
+  res.send("posted succefully");
+});
+app.delete("/test", (req, res) => {
+  res.send("deleted sucesfully");
 });
 app.listen(3000,()=>{
     console.log('server is succesfully listenng on port 3000')
