0a9c89622569cfcfbd6f29ed0aab8bc537cf33f2
NISHANT GOSWAMI <81947327+raj285@users.noreply.github.com>
2024-12-06 07:03:47 +0530

    middleware1

--- src/app.js
+++ src/app.js
@@ -3,21 +3,13 @@ const app=express();
 
   // res is necessary in qpp. use/get/post..... nhi to you will not get the answer from server
 // app.use("/ddd",res1,res2,[res3,res4],res5);
-  app.use("/user",(req,res,next)=>{
-    console.log("pehla pehla res");
-    // res.send("1st response");
-    next();
-    // above will call nect res if above res is not there 
-    //  if res is there it will not work
-    // but if res come now 
-    res.send("1st late response");
-    // above will give 2nd response late response
-    // next paused late amessage 
-    //  error bcs. after next one more res came; as request already fullfilled
-
-  },
-(req,res)=>{
+  app.get("/user",(req,res)=>{
     console.log("dusra dusra message");
     res.send("2nd respond");
 })
 app.listen(7777);
+// between get and res function there are many functions existed and they are known as middleware
+// example is authorization before giving the value of data to admin you first have to authorize him first 
+// and this authorization is middleware here 
+// in last git session functions with next() are middleware only
+// bcs. they were coming between res and get funcrion 🤩
