712bc734d1ec83865afcf8c2cbbe417076f0b9ad
NISHANT GOSWAMI <81947327+raj285@users.noreply.github.com>
2024-12-06 07:15:17 +0530

    authorization eg of middleware

--- src/app.js
+++ src/app.js
@@ -3,10 +3,20 @@ const app=express();
 
   // res is necessary in qpp. use/get/post..... nhi to you will not get the answer from server
 // app.use("/ddd",res1,res2,[res3,res4],res5);
-  app.get("/user",(req,res)=>{
-    console.log("dusra dusra message");
-    res.send("2nd respond");
+  app.get("/admin/getalldata",(req,res)=>{
+    // but before these things we have to authorize you
+    const token="xyvbvz";
+    const isautho= token==="xyz";
+    if(isautho){
+      res.send("le bhai apna data")
+    }
+    else{
+      res.status(401).send("not authorized")
+    }
 })
+  app.delete("/admin/getalldata", (req, res) => {
+    res.send("le bhai apna data maine delete kr diya");
+  });
 app.listen(7777);
 // between get and res function there are many functions existed and they are known as middleware
 // example is authorization before giving the value of data to admin you first have to authorize him first 
