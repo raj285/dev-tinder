788a720778fcf92ce0610cf9ff9c72293fc7161d
NISHANT GOSWAMI <81947327+raj285@users.noreply.github.com>
2024-12-10 07:01:01 +0530

    resding data from req

--- src/app.js
+++ src/app.js
@@ -1,28 +1,26 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
-const User=require("./models/user");
+const User = require("./models/user");
+//  we should take data from API only then update that data to database
+// find jsobject nad json?
+// adding a middleware to read all the json files
+//  if no url given then this method will work on all url.
+app.use(express.json());
 
-//  creating new instance of user model
-// read about __v ...what's that?
-app.post("/signup",async (req,res)=>{
-  const userObj = {
-    firstName: "Deepak",
-    lastName: "Goswami",
-    emailId: "goswaminishant9670@gmail.com",
-    password: "aafat794613",
-    age: 78,
-    gender:"male"
-  };
-  const user=new User(userObj);
-  //  below function will return a promise  
+// reading data present in body of request
+
+app.post("/signup", async (req, res) => {
+  console.log(req.body);
+  const user=new User(req.body);
+  // //  below function will return a promise
   try{await  user.save();
   res.send("user added successfully")}
   catch(err){
     res.status(404).send("error not found");
   }
-  
-})
+});
+
 connectDB()
   .then(() => {
     app.listen(7777, () => {
