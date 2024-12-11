5f5c5578b885f481e2c257a2f23e38ae66e4e035
NISHANT GOSWAMI <81947327+raj285@users.noreply.github.com>
2024-12-11 06:58:46 +0530

    Rest APIs l20 completed

--- src/app.js
+++ src/app.js
@@ -9,18 +9,73 @@ const User = require("./models/user");
 app.use(express.json());
 
 // reading data present in body of request
-
+//  find one will find first/older document (in case of many)documentation -> randomly)
+//  read documentation
 app.post("/signup", async (req, res) => {
   console.log(req.body);
-  const user=new User(req.body);
+  const user = new User(req.body);
   // //  below function will return a promise
-  try{await  user.save();
-  res.send("user added successfully")}
-  catch(err){
+  try {
+    await user.save();
+    res.send("user added successfully");
+  } catch (err) {
     res.status(404).send("error not found");
   }
 });
 
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  console.log(userEmail);
+  try {
+    const user = await User.find({ emailId: userEmail });
+    if (user.length === 0) {
+      res.status(404).send("user not found");
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+//  to find all the people
+app.get("/feed", async (req, res) => {
+  try {
+    const user = await User.find({});
+    if (user.length === 0) {
+      res.status(404).send("user not found");
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(404).send("something went wrong");
+  }
+});
+// to delete something
+app.delete("/user", async (req, res) => {
+  const userId1 = req.body.userId;
+  console.log(userId1);
+  try {
+    // read documentation to see for syntax
+    const user = await User.findByIdAndDelete({ _id: userId1 });
+    res.send("deleted succesfully");
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+// update data of the user
+// it don't update the fields which are not avaialble in schema
+//  no addn. of new fields
+// options rea dit from documentation
+// what u have given only that will change in the document
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data);
+    res.send("user updated succesfully");
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
 connectDB()
   .then(() => {
     app.listen(7777, () => {
